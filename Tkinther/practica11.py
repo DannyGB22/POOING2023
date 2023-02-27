ca4e39d4665cdbbfacdcd41fea8b05d839130cf2
DannyGB22 <125704277+DannyGB22@users.noreply.github.com>
2023-02-27 17:20:56 -0600

    Create practica11.py
    En esta practica vimos tkinther, donde instanciamos el objeto ventana, Agregamos Frames y los botones.

---
+++ Tkinther/practica11.py
@@ -0,0 +1,49 @@
+from tkinter import Tk,Button,Frame,messagebox
+
+def mostrarMensaje():
+    messagebox.showinfo("Aviso:", "Presionaste el boton azul")
+    
+
+
+
+#1. Instanciamos el objeto ventana 
+ventana= Tk()
+ventana.title("Ejemplo de 3 Frames ")
+ventana.geometry("600x400")
+
+
+#2. Agregamos los Frames 
+
+seccion1= Frame(ventana, bg="red")
+seccion1.pack(expand=True, fill='both')
+
+seccion2= Frame(ventana, bg="yellow")
+seccion2.pack(expand=True, fill='both')
+
+seccion3= Frame(ventana, bg="blue")
+seccion3.pack(expand=True, fill='both')
+
+
+
+#3.  Agregamos botones
+
+botonAzul=Button(seccion1, text="Boton Azul", fg="blue",command=mostrarMensaje)
+botonAzul.place(x=60,y=60)
+
+botonNegro=Button(seccion2, text="Boton Negro",fg="white",bg="black")
+botonNegro.grid(row= 0, column=0)
+
+botonAmarillo=Button(seccion2, text="Boton Amarillo", bg="#ffff4d")
+botonAmarillo.grid(row=1, column=1)
+
+botonVerde=Button(seccion3, text="Boton Verde", bg="#99e699")
+botonVerde.configure(height=2, width=10)
+botonVerde.pack()
+
+
+
+
+
+#Llamamos al main 
+ventana.mainloop()
+
