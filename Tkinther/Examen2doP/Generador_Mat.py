cd0cc1e567fda65951b955ea80f769962630a159
DannyGB22 <125704277+DannyGB22@users.noreply.github.com>
2023-03-15 17:20:08 -0600

    se creo la clase Matriculagenerador y se encapsularon los atributos

---
+++ Tkinther/Examen2doP/Generador_Mat.py
@@ -0,0 +1,10 @@
+
+class MatriculaGenerador:
+    def __init__(self, nombre, apellido_paterno, apellido_materno, anio_nacimiento, carrera):
+        self.nombre = nombre
+        self.apellido_paterno = apellido_paterno
+        self.apellido_materno = apellido_materno
+        self.anio_nacimiento = anio_nacimiento
+        self.carrera = carrera
+        
+        
